ad297565ca068a93a30c6d9e4f605c5052837320
habinezadalvan <habinezadalvan@gmail.com>
2020-10-13 08:31:18 +0200

    adding h1 in the box section

--- src/components/LandingPage/boxsection/Box.js
+++ src/components/LandingPage/boxsection/Box.js
@@ -1,26 +1,11 @@
-import React, {useEffect} from 'react';
-import {gsap} from 'gsap';
-import {ScrollTrigger} from 'gsap/ScrollTrigger';
+import React from 'react'
 
 
 const Box = () => {
 
-    const cover = React.createRef();
-
-    useEffect(() => {
-        gsap.registerPlugin(ScrollTrigger);
-        gsap.from(cover.current, {scrollTrigger: {
-            trigger: cover.current,
-            scrub: true,
-            toggleActions: "restart pause none none"
-        },
-        duration: 2,
-        y:600,
-        opacity: 0
-    })
-    }, [cover]);
     return(
         <div className="box-section">
+            <h1><span>{`< `}</span><span className="span">{`Coding `}</span><span>{`/> `}</span>Imaginations...</h1>
          </div>
     )
 }
